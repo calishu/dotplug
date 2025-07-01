35e7309ead4e36a2282f9f6aeeebba531e180a4f
calishu <danny.kretschmann@proton.me>
2025-07-01 19:19:09 +0200

    refactor(cli): changing structure and add features
    Rewrote the over-complicated command structure, switching from 'add_option' to 'add_subcommand'. I added a force mode (usable with '-f' or '--force' in front of the subcommand. I also added 2 new commands 'install <url>' (where you can download dotfiles from remote repo) and 'list' giving you the whole list of installed/imported dotfiles. (Note: nothing has functionality, this is just the CLI)

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,48 @@
 #include "CLI11.hpp"
 #include <iostream>
+#include <unistd.h>
 #include <string>
 
 int main(int argc, char** argv) {
-  CLI::App app{"Dotplug is a easy-toruse dotfile manager for linux."};
+  if (!getuid()) {
+    std::cout << "Using `sudo` is prohibited." << std::endl;
+    return 1;
+  }
+
+  CLI::App app{"Dotplug is a easy-to-use dotfile manager for linux."};
   argv = app.ensure_utf8(argv);
+  app.require_subcommand();
 
-  std::string name;
-  bool apply = false, remove = false, create_new = false;
+  std::string value; // stuff like names or links.
+  bool forced = false;
+  
 
-  auto apply_opt = app.add_flag("-a,--apply", apply, "Apply a dotfile configuration.");
-  auto remove_opt = app.add_flag("-r,--remove", remove, "Remove a dotfile configuration.");
-  auto new_opt = app.add_flag("-n,--new", create_new, "Creates a new configuration.");
+  auto list_cmd = app.add_subcommand("list", "Shows a list of all installed configurations.");
 
-  auto name_opt = app.add_option("name", name, "The name of your configuration.")
-    ->check([](const std::string& value) -> std::string {
-      return value.empty()  ? "Name cannot be empty" : "";
-    });
+  auto install_cmd = app.add_subcommand("install", "Install a dotfile configuration from remote repository.");
+  install_cmd->add_option("url", value, "The Git URL of the remote repository.")->required();
 
-  apply_opt->needs(name_opt);
-  remove_opt->needs(name_opt);
-  new_opt->needs(name_opt);
+  auto apply_cmd = app.add_subcommand("apply", "Apply a dotfile configuration.");
+  apply_cmd->add_option("name", value, "The name of the dotfile configuration you want to apply.")->required();
+
+  auto remove_cmd = app.add_subcommand("remove", "Remove a dotfile configuration.");
+  remove_cmd->add_option("name", value, "The name of the dotfile configuration you want to remove.")->required();
+
+  auto new_cmd = app.add_subcommand("new", "Creates a new dotfile configuration.");
+  new_cmd->add_option("name", value, "The name of the dotfile configuration you want to create.")->required();
+
+  app.add_flag("-f,--force", forced, "Force the action and ignore every possible warning. (CAUTION!)");
+
 
   CLI11_PARSE(app, argc, argv);
 
-  
-  if (apply)
-    std::cout << "Apply config: " << name << std::endl;
-  else if (remove)
-    std::cout << "Remove config: " << name << std::endl;
-  else if (create_new)
-    std::cout << "Create config: " << name << std::endl;
-  else
-    std::cout << "No Action provided.";
+  if (forced) {
+    std::string proceed;
+    std::cout << "Are you sure you want to continue and force the actions? (y/N) ";
+    std::getline(std::cin, proceed);
 
+    if(proceed == "N" || proceed == "n" || proceed.empty()) return 0;
+  }
 
   return 0;
 }
