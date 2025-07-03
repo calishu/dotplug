a9715c536373fe6fb82c8485288fffc3e2c3145c
calishu <danny.kretschmann@proton.me>
2025-07-03 21:28:52 +0200

    deleted broken and not used files

--- src/headers/cli_utils.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "CLI11.hpp"
-
-CLI::Option* add_text_option(CLI::Option_group* group, std::string& value, const std::string& name, const std::string& description) 
--- src/utils/cli_utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "cli_utils.hpp"
-
-CLI::Option* add_text_option(CLI::Option_group* group, std::string& value, const std::string& name, const std::string& description) {
-  return group->add_option(name, value, description)
-    ->check([](const std::string& val) -> std::string {
-      return value.empty() ? name + " cannot be empty" : "";
-  });
-}
