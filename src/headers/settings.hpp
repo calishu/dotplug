94ee17f7d5a91c8c42fa1dfdb03643460c041636
calishu <danny.kretschmann@proton.me>
2025-07-07 23:21:04 +0200

    feat: add destination path

--- src/headers/settings.hpp
+++ src/headers/settings.hpp
@@ -3,4 +3,5 @@
 #include <cstdlib>
 #include <string>
 
-inline std::string dotfiles_path = std::string(std::getenv("HOME")) + DOTFILES_PATH;
+inline std::string dotfiles_path    = std::string(std::getenv("HOME")) + DOTFILES_PATH;
+inline std::string destination_path = std::string(std::getenv("HOME")) + DESTINATION_PATH;
