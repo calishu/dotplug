1b43834861fbce13434087183640518601a75bb0
calishu <danny.kretschmann@proton.me>
2025-07-19 22:48:50 +0200

    feat(config): config class scaffold
    I just implemented the scaffold, with the 'parse_config' and 'get_dependencies()' function inside it.

--- src/utils/config.cpp
+++ src/utils/config.cpp
@@ -4,65 +4,51 @@
 #include <filesystem>
 #include <iostream>
 #include <string>
+#include <vector>
 
-std::pair<toml::table, int> parse_config(const std::string& name) { 
-  toml::table config;
+class Config {
+private:
+  std::string name_;
+  toml::table config_;
 
-  std::string path = dotfiles_path + name + "/config.toml";
-  if (!std::filesystem::exists(path)) {
-    std::cerr << "The config doesn't have a valid config.toml!" << std::endl;
-    return {toml::table{}, 1};
-  }
+  void parse_config(std::string name) {
+    std::string path = dotfiles_path + name + "/config.toml";
 
-  try {
-    config = toml::parse_file(path);
-  } catch (const toml::parse_error& err) {
-    std::cerr << "Parsing failed: \n" << err << std::endl;
-    return {toml::table{}, 1};
-  }
-
-  return {config, 0};
-}
-
-// TODO: Make it better.
-// Somehow I feel ashamed of writing this piece of code.
-// I did a lot of bad code, but idk why I use global pointers for that.
-std::pair<std::string, int> dep_source(const toml::table& config, const std::string& dep_name) {
-  auto source_node = config["dotplug"][dep_name]["source"];
-  std::string source_path = source_node.is_string() ? source_node.value_or("") : "";
-
-  std::filesystem::path source = source_path.empty()
-    ? dotfiles_path + ctx->name + "/" + dep_name
-    : dotfiles_path + ctx->name + "/" + source_path;
-
-  if (std::filesystem::exists(source) && std::filesystem::is_directory(source))
-    return { std::filesystem::absolute(source), 0 };
-  return { "", 1 };
-}
-
-void print_config(toml::table& config, const std::string prefix) {
-  std::string_view name = config["dotplug"]["name"].value_or(std::string_view("Unknown Name"));
-  std::string_view description = config["dotplug"]["description"].value_or(std::string_view(""));
-  std::string_view author = config["dotplug"]["author"].value_or(std::string_view(""));
-  auto dependencies = config["dotplug"]["dependencies"];
-
-  std::cout << (!prefix.empty() ? "[" + prefix + "] " : "") << name << ":" << std::endl;
-  if (!description.empty()) std::cout << "        Description: " << description << std::endl;
-  if (!author.empty()) std::cout << "        Author: " << author << std::endl;
-  
-  if (toml::array* arr = dependencies.as_array()) {
-    if (!arr->empty()) {
-      std::cout << "        Dependencies: ";
-      
-      for (size_t y = 0; y < arr->size(); ++y) {
-        const auto& el = (*arr)[y];
-
-        if (el.is_string()) {
-          std::cout << *el.value<std::string>();
-          if (y != arr->size() - 1) std::cout << ", ";
-        }
-      }
-      std::cout << std::endl;
+    if(!std::filesystem::exists(path)) {
+      std::cerr << "The config doesn't have a config.toml" << std::endl;
+      return;
     }
+
+    try {
+      config_ = toml::parse_file(path);
+    } catch (const toml::parse_error& err) {
+      std::cerr << "Parsing failed: \n" << err << std::endl;
+    }
+    return;
+  }
+
+public:
+  Config(const std::string& name) : name_(name) {
+    parse_config(name);
+  }
+
+  const std::vector<std::string>& get_dependencies() {
+    static std::vector<std::string> dependencies;
+    dependencies.clear();
+
+    const auto& config_deps = config["dotplug"]["dependencies"];
+    const auto* deps_array  = config_deps.as_array();
+
+    if (!deps_array || deps_array->empty())
+      return dependencies;
+
+    for (size_t i = 0; i < deps_array->size(); ++i) {
+      const auto& dep = (*arr)[i];
+
+      if (!dep.is_string()) continue;
+      dependencies.push_back(dep);
+    }
+
+    return dependencies;
   }
 }
