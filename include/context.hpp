da528301dcb9299c5c4edf0d07c31f0bc9287b16
calishu <danny.kretschmann@proton.me>
2025-08-02 02:33:10 +0200

    fix(context): added `locale` and `logging`
    Forgot to commit it, sorry.

--- include/context.hpp
+++ include/context.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include "utils/lang.hpp"
+#include "utils/logging.hpp"
 #include <string>
 
 struct Context {
     std::string name;
+    Logging *logging;
+    locale_data locale;
     bool forced;
 };
 
