965e45990f12d5ec8ed3e1d46e68679075e13153
calishu <danny.kretschmann@proton.me>
2025-08-03 20:55:18 +0200

    refactor/fix(validation): fixed a logic bug and refactored it to the new logging system

--- src/modules/validator.cpp
+++ src/modules/validator.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
+#include "context.hpp"
 #include "modules/validator.hpp"
+#include "utils/logging.hpp"
 
 auto ValidationResult::has_errors() const -> bool {
     for (const auto &e : log)
-        if (e.type == LogEntry::Type::Error)
+        if (e.level_ == LoggingLevel::ERROR)
             return true;
     return false;
 }
 
 auto ValidationResult::has_warnings() const -> bool {
     for (const auto &e : log)
-        if (e.type == LogEntry::Type::Warning)
+        if (e.level_ == LoggingLevel::WARNING)
             return true;
     return false;
 }
 
 auto ValidationResult::add_error(const std::string &msg) -> void {
-    log.emplace_back(msg, LogEntry::Type::Error);
+    log.emplace_back(msg, LoggingLevel::ERROR);
 }
 
 auto ValidationResult::add_warning(const std::string &msg) -> void {
-    log.emplace_back(msg, LogEntry::Type::Warning);
+    log.emplace_back(msg, LoggingLevel::WARNING);
 }
 
 auto ValidationResult::print() const -> void {
-    if (log.empty())
-        std::cout << "Everything is fine with the configuration.\n";
+    if (log.empty()) {
+        ctx->logging->log(LoggingLevel::INFO, ctx->locale.json["validation"]["statements"]["success"]);
+        return;
+    }
+
     for (const auto &e : log)
-        std::cout << (e.type == LogEntry::Type::Error ? "[ERROR] " : "[WARNING] ") << e.message << "\n";
-    return;
+        if (e.level_ == LoggingLevel::ERROR)
+            ctx->logging->log(LoggingLevel::ERROR, e.message);
+        else if (e.level_ == LoggingLevel::WARNING)
+            ctx->logging->log(LoggingLevel::WARNING, e.message);
 }
